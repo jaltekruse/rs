099c3956787bf6ed47712f9ad9fe227d3c835cbd
Jason Altekruse <altekrusejason@gmail.com>
2023-09-18 16:08:44 -0500

    WIP - just getitng stuff running, disabled Latex

--- build.py
+++ build.py
@@ -241,8 +241,8 @@ with Live(generate_table(status), refresh_per_second=4) as lt:
             "docker-compose.yml",
             "-f",
             "author.compose.yml",
-            "--progress",
-            "plain",
+            # "--progress",
+            # "plain",
             "build",
             service,
         ]
